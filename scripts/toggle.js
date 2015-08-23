4b189f07a516d8de1819bf1d01b7891afa3298dd
chandrikamathi <chandrika.mathi@gmail.com>
2015-08-23 19:02:40 -0400

    initial commit

---
+++ scripts/toggle.js
@@ -0,0 +1,15 @@
+/**
+ * Created by admin on 8/23/2015.
+ */
+document.getElementById("Loginclick").onclick=function(){
+
+    var el = document.getElementById('slide');
+    if (el.style.display == 'block'){
+        el.style.display = 'none';
+
+    }else{
+        el.style.display = 'block';
+
+    }
+
+};
